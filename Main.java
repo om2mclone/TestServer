ca1851dbd799e2a9255a1562aedc36fc15b2c9b1
Olgun Cengiz <olgun.cengiz@ericsson.com>
2014-12-03 16:19:13 +0200

    Files

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 import com.db4o.Db4o;
 import com.db4o.ObjectServer;
 
 @SuppressWarnings("deprecation")
 public class Main {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		System.out.println("TestServer Started");
 		
@@ -14,9 +19,13 @@ public class Main {
 		
 		System.out.println("Server Created");
 		
-		while(true)
+		String choice = "";
+		Scanner in = new Scanner(System.in);
+		while(!choice.equals("0"))
 		{
-			
+			System.out.println("Press '0' for exit");
+			choice = in.nextLine().trim();
 		}
+		in.close();
 	}
 }
